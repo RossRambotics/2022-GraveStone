b169e006da99b8c558a5ae5a3f6e596ee3801cac
ChesterParker3201 <Chester.j.parker@gmail.com>
2022-01-29 12:48:46 -0500

    added comment

--- src/main/java/frc/robot/commands/DriveAuto.java
+++ src/main/java/frc/robot/commands/DriveAuto.java
@@ -14,15 +14,19 @@ public class DriveAuto extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    // do something great
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
