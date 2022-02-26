36051db746e6186735d6861f56ce13cd1adbc6bb
hollisms <info@spyschuler.com>
2022-02-26 17:30:53 -0500

    fixing merge changes from pull #9

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -66,10 +66,7 @@ public final class Constants {
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 7; // FIXME Set back right steer encoder ID
     public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(171.6339 + MASTER_STEER_OFFSET);
 
-    // right steer offset
-
-    public static final int SHOOTER_MOTOR_FRONT = 31; // CAN ID of the front shooter motor
-    public static final int SHOOTER_MOTOR_BACK = 32; // CAN ID of the back shooter motor
+    // right steer offse
 
     public static final int TURRET_MOTOR = 35; // CAN ID of the yaw turret motor
     public static final int ANGULAR_MOTOR = 36; // CAN ID of the angular (pitch) motor on the turret
