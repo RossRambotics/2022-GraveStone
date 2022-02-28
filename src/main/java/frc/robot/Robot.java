45b461bf42941ac0ed5e0dd688d9549a0781a630
hollisms <info@spyschuler.com>
2022-02-28 07:46:24 -0500

    added auton chooser to auto tab

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -78,6 +78,7 @@ public class Robot extends TimedRobot {
 
         // schedule the autonomous command (example)
         if (m_autonomousCommand != null) {
+            System.out.println("Auto Command: " + m_autonomousCommand.getName());
             m_autonomousCommand.schedule();
         }
     }
