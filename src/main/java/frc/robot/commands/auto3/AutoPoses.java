363bfa442255447225887e1bd02ccc517e2463a5
Ben Schuler <ben@spyschuler.com>
2022-04-07 14:35:29 -0500

    1 Ball Auto, Start Pose

--- src/main/java/frc/robot/commands/auto3/AutoPoses.java
+++ src/main/java/frc/robot/commands/auto3/AutoPoses.java
@@ -19,66 +19,70 @@ import frc.robot.commands.Shooter.ShootHigh;
 
 /** Add your docs here. */
 public class AutoPoses {
-    public final static Pose2d S1 = new Pose2d(6.53, 5.37, new Rotation2d(Math.toRadians(-41.01)));
-    public final static Pose2d S2 = new Pose2d(7.03, 4.57, new Rotation2d(Math.toRadians(-22.11)));
-    public final static Pose2d S3 = new Pose2d(7.7, 2.75, new Rotation2d(Math.toRadians(69)));
-    public final static Pose2d C1 = new Pose2d(5.77, 5.86, new Rotation2d(Math.toRadians(-22.84)));
-    public final static Pose2d C2 = new Pose2d(5.81, 2.31, new Rotation2d(Math.toRadians(29.75)));
-    public final static Pose2d C3 = new Pose2d(7.32, 1.68, new Rotation2d(Math.toRadians(90.00)));
-    public final static Pose2d C4 = new Pose2d(1.77, 1.66, new Rotation2d(Math.toRadians(45.0)));
-    public final static Pose2d C5 = new Pose2d(1.77, 1.66, new Rotation2d(Math.toRadians(45.0)));
-    public final static Pose2d W1 = new Pose2d(5.54, 5.17, new Rotation2d(Math.toRadians(-17.98)));
-    public final static Pose2d W2 = new Pose2d(4.46, 4.1, new Rotation2d(Math.toRadians(3.91)));
-    public final static Pose2d W3 = new Pose2d(7.19, 1.36, new Rotation2d(Math.toRadians(70.94)));
+        public final static Pose2d S1 = new Pose2d(6.53, 5.37, new Rotation2d(Math.toRadians(-41.01)));
+        public final static Pose2d S2 = new Pose2d(7.03, 4.57, new Rotation2d(Math.toRadians(-22.11)));
+        public final static Pose2d S3 = new Pose2d(7.7, 2.75, new Rotation2d(Math.toRadians(69)));
+        public final static Pose2d C1 = new Pose2d(5.77, 5.86, new Rotation2d(Math.toRadians(-22.84)));
+        public final static Pose2d C2 = new Pose2d(5.81, 2.31, new Rotation2d(Math.toRadians(29.75)));
+        public final static Pose2d C3 = new Pose2d(7.32, 1.68, new Rotation2d(Math.toRadians(90.00)));
+        public final static Pose2d C4 = new Pose2d(1.77, 1.66, new Rotation2d(Math.toRadians(45.0)));
+        public final static Pose2d C5 = new Pose2d(1.77, 1.66, new Rotation2d(Math.toRadians(45.0)));
+        public final static Pose2d W1 = new Pose2d(5.54, 5.17, new Rotation2d(Math.toRadians(-17.98)));
+        public final static Pose2d W2 = new Pose2d(4.46, 4.1, new Rotation2d(Math.toRadians(3.91)));
+        public final static Pose2d W3 = new Pose2d(7.19, 1.36, new Rotation2d(Math.toRadians(70.94)));
+        public final static Pose2d W4 = new Pose2d(6.08, 1.21, new Rotation2d(Math.toRadians(52.74)));
 
-    public static CommandBase CaptureCargo(Pose2d pose) {
-        CommandBase cmd = new SnapDriveToPoseField(
-                RobotContainer.m_drivetrainSubsystem,
-                pose,
-                0.2);
+        public static CommandBase CaptureCargo(Pose2d pose) {
+                CommandBase cmd = new SnapDriveToPoseField(
+                                RobotContainer.m_drivetrainSubsystem,
+                                pose,
+                                0.2);
 
-        cmd = cmd.andThen(new ExtendIntake().withTimeout(0.01))
-                .andThen(new StartIntake().withTimeout(0.01))
-                .andThen(new SnapDriveToCargo(
-                        RobotContainer.m_drivetrainSubsystem,
-                        new Rotation2d()));
+                cmd = cmd.andThen(new ExtendIntake().withTimeout(0.01))
+                                .andThen(new StartIntake().withTimeout(0.01))
+                                .andThen(new SnapDriveToCargo(
+                                                RobotContainer.m_drivetrainSubsystem,
+                                                new Rotation2d()));
 
-        return cmd;
-    }
-
-    public static CommandBase ShootHigh(Pose2d pose) {
-        CommandBase cmd = new SnapDriveToPoseField(
-                RobotContainer.m_drivetrainSubsystem,
-                pose,
-                0.2);
-
-        cmd = cmd.andThen(new ShootHigh());
-
-        return cmd;
-    }
-
-    public static void SetStartPose(Pose2d pose) {
-        // RobotContainer.m_drivetrainSubsystem.zeroGyroscope();
-        DataLogManager
-                .log("Setting Start Pose: Current Gyro: "
-                        + RobotContainer.m_drivetrainSubsystem.getGyroscopeRotation());
-        RobotContainer.m_drivetrainSubsystem.setGyroScope(pose.getRotation().getDegrees());
-        DataLogManager
-                .log("Setting Start Pose: Updated Gyro: "
-                        + RobotContainer.m_drivetrainSubsystem.getGyroscopeRotation());
-        // RobotContainer.m_drivetrainSubsystem.resetOdometry();
-        DataLogManager.log(
-                "Setting Start Pose: Odometry Orignal Pose: " + RobotContainer.m_drivetrainSubsystem.getOdometryPose());
-
-        if (Robot.isSimulation()) {
-            RobotContainer.m_drivetrainSubsystem.getOdometry().resetPosition(pose,
-                    new Rotation2d(Math.toRadians(69.0)));
-        } else {
-            RobotContainer.m_drivetrainSubsystem.getOdometry().resetPosition(pose,
-                    pose.getRotation()); // uses pose, not gyro because gyro update above is async and may not be
-                                         // updated by now
+                return cmd;
         }
-        DataLogManager.log(
-                "Setting Start Pose: Odometry Updated Pose: " + RobotContainer.m_drivetrainSubsystem.getOdometryPose());
-    }
+
+        public static CommandBase ShootHigh(Pose2d pose) {
+                CommandBase cmd = new SnapDriveToPoseField(
+                                RobotContainer.m_drivetrainSubsystem,
+                                pose,
+                                0.2);
+
+                cmd = cmd.andThen(new ShootHigh());
+
+                return cmd;
+        }
+
+        public static void SetStartPose(Pose2d pose) {
+                // RobotContainer.m_drivetrainSubsystem.zeroGyroscope();
+                DataLogManager
+                                .log("Setting Start Pose: Current Gyro: "
+                                                + RobotContainer.m_drivetrainSubsystem.getGyroscopeRotation());
+                RobotContainer.m_drivetrainSubsystem.setGyroScope(pose.getRotation().getDegrees());
+                DataLogManager
+                                .log("Setting Start Pose: Updated Gyro: "
+                                                + RobotContainer.m_drivetrainSubsystem.getGyroscopeRotation());
+                // RobotContainer.m_drivetrainSubsystem.resetOdometry();
+                DataLogManager.log(
+                                "Setting Start Pose: Odometry Orignal Pose: "
+                                                + RobotContainer.m_drivetrainSubsystem.getOdometryPose());
+
+                if (Robot.isSimulation()) {
+                        RobotContainer.m_drivetrainSubsystem.getOdometry().resetPosition(pose,
+                                        new Rotation2d(Math.toRadians(69.0)));
+                } else {
+                        RobotContainer.m_drivetrainSubsystem.getOdometry().resetPosition(pose,
+                                        pose.getRotation()); // uses pose, not gyro because gyro update above is async
+                                                             // and may not be
+                                                             // updated by now
+                }
+                DataLogManager.log(
+                                "Setting Start Pose: Odometry Updated Pose: "
+                                                + RobotContainer.m_drivetrainSubsystem.getOdometryPose());
+        }
 }
--- src/main/java/frc/robot/commands/auto3/S2_W2_High.java
+++ src/main/java/frc/robot/commands/auto3/S2_W2_High.java
@@ -10,9 +10,9 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.Intake.ResetIntake;
 import frc.robot.commands.Shooter.ShootLow;
 
-public class S1_C1_High extends CommandBase {
+public class S2_W2_High extends CommandBase {
     /** Creates a new S3_Low_C3_High. */
-    public S1_C1_High() {
+    public S2_W2_High() {
         // Use addRequirements() here to declare subsystem dependencies.
     }
 
@@ -23,14 +23,12 @@ public class S1_C1_High extends CommandBase {
         DataLogManager.log("Auto command: " + this.getName());
 
         // Set Starting Pose
-        AutoPoses.SetStartPose(AutoPoses.S1);
+        AutoPoses.SetStartPose(AutoPoses.S2);
 
         // Create command group for the auto routine
         SequentialCommandGroup command = new SequentialCommandGroup(
                 new ResetIntake().withTimeout(0.2),
-                AutoPoses.CaptureCargo(AutoPoses.C1).withTimeout(7.0),
-                AutoPoses.ShootHigh(AutoPoses.C1).withTimeout(5.0));
-
+                AutoPoses.ShootHigh(AutoPoses.W2).withTimeout(10.0));
         command.schedule();
     }
 
