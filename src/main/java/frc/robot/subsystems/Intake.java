329c64967b2b35da8ab9a7daeb2f60b9a624de75
hollisms <info@spyschuler.com>
2022-04-08 11:19:40 -0500

    default now blue

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -82,8 +82,8 @@ public class Intake extends SubsystemBase {
 
         m_extensionMotor.configNominalOutputForward(0, IntakeConstants.kTimeoutMs);
         m_extensionMotor.configNominalOutputReverse(0, IntakeConstants.kTimeoutMs);
-        m_extensionMotor.configPeakOutputForward(0.1, IntakeConstants.kTimeoutMs);
-        m_extensionMotor.configPeakOutputReverse(-0.1, IntakeConstants.kTimeoutMs);
+        m_extensionMotor.configPeakOutputForward(0.2, IntakeConstants.kTimeoutMs);
+        m_extensionMotor.configPeakOutputReverse(-0.2, IntakeConstants.kTimeoutMs);
 
         /* Config the Velocity closed loop gains in slot0 */
         m_extensionMotor.config_kF(IntakeConstants.kPIDLoopIdx, m_gainsVelocity.kF, IntakeConstants.kTimeoutMs);
